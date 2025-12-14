c05e206519d31bd174d20da815a80c7944b1ea61
Alan Zhang <allan.lu.zhang@gmail.com>
2025-12-14 15:38:49 -0600

    Restore imports and dashboard setup in dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -65,7 +65,7 @@ st.sidebar.markdown("---")
 st.sidebar.markdown("**Credits**")
 st.sidebar.caption("Created by **Alan Zhang**")
 st.sidebar.caption("Data Source: [U.S. Bureau of Labor Statistics (BLS)](https://www.bls.gov/)")
-st.sidebar.caption(f"small:[Dashboard last updated on {refreshed_date}]")
+st.sidebar.caption(f"Dashboard last updated on {refreshed_date}")
 
 #filtered data
 df_plot = df.query(("Date >= @start_date and Date <= @end_date")).copy()
@@ -240,6 +240,7 @@ elif st.session_state.page_view == "Work Hours & Pay":
             })
 
         )
+
 
 
 
